315b29c2a57bf75e38c95037492e8860e61594d2
mark-boer <m.h.boer.2@gmail.com>
2021-05-03 16:27:15 +0200

    fix version for python>3.7

--- geomfitty/__init__.py
+++ geomfitty/__init__.py
@@ -1,3 +1,6 @@
-from importlib_metadata import version
+try:
+    from importlib.metadata import version  # type: ignore
+except ImportError:
+    from importlib_metadata import version  # type: ignore
 
 __version__ = version(__package__)
